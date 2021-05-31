0f9dc16402403dea546c855050adcb96c34fa05a
JL-OLEMAR <joseluis19963@gmail.com>
2021-05-31 12:08:03 -0500

    Registro de Tweets

--- handlers/handlers.go
+++ handlers/handlers.go
@@ -14,10 +14,15 @@ import (
 /*Manejadores seteo mi puerto, el Handler y pongo a escuchar al Servidor */
 func Manejadores() {
 	router := mux.NewRouter()
+
+	// EndPoints de usuario
 	router.HandleFunc("/registro", middlewares.ChequeoBD(routers.Registro)).Methods("POST")
 	router.HandleFunc("/login", middlewares.ChequeoBD(routers.Login)).Methods("POST")
 	router.HandleFunc("/verperfil", middlewares.ChequeoBD(middlewares.ValidoJWT(routers.VerPerfil))).Methods("GET")
 	router.HandleFunc("/modificarPerfil", middlewares.ChequeoBD(middlewares.ValidoJWT(routers.ModificarPerfil))).Methods("PUT")
+
+	// EndPoints de tweet
+	router.HandleFunc("/tweet", middlewares.ChequeoBD(middlewares.ValidoJWT(routers.GraboTweet))).Methods("POST")
 
 	PORT := os.Getenv("PORT")
 	if PORT == "" {
